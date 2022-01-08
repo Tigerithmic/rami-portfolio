ab05863f97477ce06b2409b5a9419d403ec37890
subrami <subramib@gmail.com>
2022-01-08 17:52:32 -0500

    nav changes

--- components/navbar.js
+++ components/navbar.js
@@ -20,6 +20,18 @@ import {
 import { HamburgerIcon } from '@chakra-ui/icons';
 import ToggleThemeButton from './toggle-theme-button';
 
+const darkGradient = `repeating-linear-gradient(-65deg,
+  #222,
+  #222 40px,
+  #2C2C2A 70px,
+  #2C2C2A 30px)`;
+
+const lightGradient = `repeating-linear-gradient(-65deg,
+  #DECFC0,
+  #DECFC0 40px,
+  #E9DCD0 70px,
+  #E9DCD0 30px)`;
+
 const LinkItems = ({ href, path, children }) => {
   const active = path === href;
   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900');
@@ -40,23 +52,20 @@ const Navbar = (props) => {
   const { path } = props;
 
   return (
-    <Box
-      position="fixed"
-      as="nav"
-      w="100%"
-      bg={useColorModeValue('#fffffff40', '#20202380')}
-      style={{ backdropFilter: 'blur(10px)' }}
-      zIndex={1}
-      overflow="visible"
-      {...props}
-    >
+    <Box as="nav" borderRadius={30} w="100%">
       <Container
         display="flex"
-        p={2}
+        p={1}
+        borderRadius="lg"
         maxW="container.md"
         wrap="wrap"
         align="center"
         justify="space-between"
+        bg={useColorModeValue(lightGradient, darkGradient)}
+        style={{ backdropFilter: 'blur(10px)' }}
+        zIndex={1}
+        overflow="visible"
+        {...props}
       >
         <Flex align="center" marginRight={5}>
           <Heading as="h1" size="lg" letterSpacing={'tighter'}>
